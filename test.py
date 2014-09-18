a231296b131ca6f4f7c44f6db869122def67b8b5
eight <eight04@gmail.com>
2014-09-18 09:33:46 +0800

    remove cache and fix bugs

--- test.py
+++ test.py
@@ -2,27 +2,13 @@
 
 from worker import Worker
 
-class Test(Worker):
-	def worker(self):
-		i = 1
-		while True:
-			print(i)
-			self.wait(1)
-			i += 1
-			
-test = Test().start()
-while True:
-	s = input()
-	if s == "p":
-		test.pause()
-	if s == "r":
-		test.resume()
-	if s == "q":
-		test.stop()
-		break
-	if s == "s":
-		test.stop()
-	if s == "e":
-		print(test.error.empty() or test.error.get_nowait())
-	if s == "rs":
-		test.start()
+def raiseMe(s):
+	raise Exception(s)
+
+class Main(Worker):
+	def __init__(self):
+		super().__init__()
+		
+		self.waitChild(raiseMe, "Hello error!")
+
+Main()
