48477f423bfbd0552eeb69189b36eee00e62d9d3
eight <eight04@gmail.com>
2015-06-08 07:21:36 +0800

    Use new syntax

--- comiccrawler/mods/iibq.py
+++ comiccrawler/mods/iibq.py
@@ -8,38 +8,31 @@ Ex:
 """
 
 import re, execjs
-from comiccrawler import Episode, grabhtml
+from ..core import Episode, grabhtml
 from html import unescape
 from safeprint import safeprint
 from functools import partial
 
 domain = ["iibq.com"]
 name = "精明眼"
-noepfolder = False
-header = {}
 
-def gettitle(html, **kw):
+def gettitle(html, url):
 	title = re.search(r"<h1[^>]*>(.+?)</h1>", html, re.DOTALL).group(1)
 	return title.strip()
 	
-def getepisodelist(html, url=""):
+def getepisodelist(html, url):
 	s = []
 	html = html[html.index('<div class="cVol">'):]
 	base = re.search("(https?://[^/]+)", url).group(1)
-	for match in re.finditer(r"href='(http://www\.iibq\.com/comic/\d+/viewcomic\d+/)'>([^<]+)", html):
+	for match in re.finditer(
+			r"href='(http://www\.iibq\.com/comic/\d+/viewcomic\d+/)'>([^<]+)",
+			html):
 		u, title = match.groups()
-		u = match.group(1)
-		title = match.group(2)
-		e = Episode()
-		e.title = title
-		e.firstpageurl = u
-		s.append(e)
+		s.append(Episode(title, u))
 			
 	return s[::-1]
 
-def getimgurls(html, url=""):
-	header["Referer"] = url
-	
+def getimgurls(html, url):
 	sFiles = re.search('sFiles="([^"]+)"', html).group(1)
 	sPath = re.search('sPath="([^"]+)"', html).group(1)
 	
@@ -71,10 +64,3 @@ def getimgurls(html, url=""):
 	SLUrl = re.search('sDS = "([^"]+)"', ds).group(1).split("^")[0].split("|")[1]
 	
 	return [SLUrl + sPath + f for f in arrFiles]
-
-def errorhandler(er, ep):
-	pass
-	
-def getnextpageurl(pagenumber, html, url=""):
-	pass
-	
