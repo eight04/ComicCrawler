ab7a90f443f1b8241395eb8aad0db326cc53be79
eight <eight04@gmail.com>
2015-07-15 12:15:34 +0800

    Fix re and js bug

--- comiccrawler/mods/seemh.py
+++ comiccrawler/mods/seemh.py
@@ -10,8 +10,9 @@ Ex:
 
 import re, execjs
 
+from urllib.parse import urljoin
+
 from ..core import Episode, grabhtml
-from ..safeprint import safeprint
 
 domain = ["tw.seemh.com", "www.seemh.com"]
 name = "看漫畫"
@@ -21,9 +22,12 @@ def gettitle(html, url):
 
 def getepisodelist(html, url):
 	cid = re.search(r"comic/(\d+)", url).group(1)
-	ep_re = r'href="(/comic/{}/\d+\.html)" title="[^"]+"'.format(cid)
+	ep_re = r'href="(/comic/{}/\d+\.html)" title="([^"]+)"'.format(cid)
 	arr = []
+	comment_pos = html.index('class="comment-bar"')
 	for match in re.finditer(ep_re, html):
+		if match.start() >= comment_pos:
+			break
 		ep_url, title = match.groups()
 		arr.append(Episode(title, urljoin(url, ep_url)))
 	return arr[::-1]
@@ -34,13 +38,14 @@ def getimgurls(html, url):
 		html
 	).group()
 	configjs = grabhtml(configjs_url, referer=url)
-	crypto = re.search(r"(var CryptoJS.+?)var pVars", configjs, re.M).group(1)
+	crypto = re.search(r"(var CryptoJS.+?)var pVars", configjs, re.S).group(1)
 
 	info_eval = re.search(
 		r'<script type="text/javascript">(eval[^<]+)',
 		html
 	).group(1)
 
-	files, path = execjs.eval(crypto + info_eval + "[cInfo.files, cInfo.path];")
+	ctx = execjs.compile(crypto + info_eval)
+	files, path = ctx.eval("[cInfo.files, cInfo.path]")
 
 	return ["http://i.seemh.com:88" + path + file for file in files]
