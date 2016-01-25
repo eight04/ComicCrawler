204246cfb92c23236e309923544a4f6b32c90b99
eight <eight04@gmail.com>
2016-01-26 01:48:38 +0800

    Fix re pattern

--- comiccrawler/mods/facebook.py
+++ comiccrawler/mods/facebook.py
@@ -4,6 +4,7 @@
 
 Ex:
 	https://www.facebook.com/gtn.moe/photos/pcb.942148229165656/942147902499022/?type=3&theater
+	https://www.facebook.com/photo.php?fbid=10202533447211476&set=a.10202533447131474.1073741835.1654599523&type=3&theater
 
 """
 
@@ -18,7 +19,10 @@ circular = True
 noepfolder = True
 
 def gettitle(html, url):
-	id = re.search(r"photos/([^/]+)", url).group(1)
+	try:
+		id = re.search(r"photos/([^/]+)", url).group(1)
+	except AttributeError:
+		id = re.search("set=([^&]+)", url).group(1)
 	title = re.search("<title[^>]*>([^<]+)", html).group(1)
 	return unescape("{} ({})".format(title, id))
 
@@ -26,7 +30,10 @@ def getepisodelist(html, url):
 	return [Episode("image", url)]
 
 def getimgurl(html, url, page):
-	id = re.search(r"photos/[^/]+/(\d+)", url).group(1)
+	try:
+		id = re.search(r"photos/[^/]+/(\d+)", url).group(1)
+	except AttributeError:
+		id = re.search("fbid=([^&]+)", url).group(1)
 	return urljoin(url, "/photo/download/?fbid=" + id)
 
 def getnextpageurl(html, url, page):
