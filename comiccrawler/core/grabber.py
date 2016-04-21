52d1011894824decc08aaf4f9bceb4d45fb2e19e
eight <eight04@gmail.com>
2016-04-21 14:24:15 +0800

    Fix undefined error

--- comiccrawler/core/grabber.py
+++ comiccrawler/core/grabber.py
@@ -13,6 +13,7 @@ from worker import sync, sleep
 from pprint import pformat
 
 from ..config import setting
+from ..io import content_write
 
 default_header = {
 	"User-Agent": "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:34.0) Gecko/20100101 Firefox/34.0",
