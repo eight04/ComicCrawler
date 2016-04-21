c1498213c10d2ee9ef6da31e643f46bcbd6d5973
eight <eight04@gmail.com>
2016-04-21 15:27:27 +0800

    Move to requests

--- comiccrawler/core/grabber.py
+++ comiccrawler/core/grabber.py
@@ -2,12 +2,15 @@
 
 import gzip
 import re
+import requests
 
 from urllib.parse import quote, urlsplit, urlunsplit
 from urllib.request import Request, urlopen
-from urllib.error import HTTPError
+# from urllib.error import HTTPError
 
 from http.cookies import SimpleCookie
+
+from requests.exceptions import HTTPError
 
 from worker import sync, sleep
 from pprint import pformat
@@ -51,98 +54,58 @@ def safeheader(header):
 			)
 		header[key] = quote_unicode(value)
 	return header
+	
+def grabber_log(*args):
+	content_write("~/comiccrawler/grabber.log", pformat(args) + "\n\n", append=True)
 
-cookiejar = {}
-def grabber(url, header=None, *, referer=None, cookie=None, raw=False, errorlog=None):
+sessions = {}
+def grabber(url, header=None, *, referer=None, cookie=None, raw=False):
 	"""Request url, return text or bytes of the content."""
+	scheme, netloc, path, query, frag = urlsplit(url)
+	
+	if netloc not in sessions:
+		s = requests.Session()
+		s.headers.update(default_header)
+		sessions[netloc] = s
+	else:
+		s = sessions[netloc]
+		
+	if header:
+		s.headers.update(header)
 
-	url = safeurl(url)
-
-	print("[grabber]", url, "\n")
-
-	# Build request
-	request = Request(url)
-
-	# Build header
-	if header is None:
-		header = {}
-
-	# Build default header
-	for key in default_header:
-		if key not in header:
-			header[key] = default_header[key]
-
-	# Referer
 	if referer:
-		header["Referer"] = referer
-
-	# Handle cookie
-	if request.host not in cookiejar:
-		cookiejar[request.host] = SimpleCookie()
-
-	jar = cookiejar[request.host]
+		s.headers['referer'] = referer
 
 	if cookie:
-		jar.load(cookie)
+		requests.utils.add_dict_to_cookiejar(s.cookies, cookie)
 
-	if "Cookie" in header:
-		jar.load(header["Cookie"])
-
-	if jar:
-		header["Cookie"] = "; ".join([key + "=" + c.value for key, c in jar.items()])
-
-	header = safeheader(header)
-	for key, value in header.items():
-		request.add_header(key, value)
-
-	response = None
-	while not response:
-		try:
-			response = urlopen(request, timeout=20)
-		except HTTPError as err:
-			if err.code == 429:
-				retry = 20
-				for key, value in err.headers.items():
-					if key == "Retry-After":
-						retry = int(value)
-						break
-				sleep(retry)
+	while True:
+		r = s.get(url, timeout=20)
+		if r.status_code == 200:
+			break
+			
+		if r.status_code == 429:
+			if "retry-after" in r.headers:
+				retry = r.headers['retry-after']
 			else:
-				raise
+				retry = 20
+			sleep(retry)
+		else:
+			r.raise_for_status()
+			
+	if setting.getboolean("errorlog"):
+		grabber_log(url, r.request.headers, r.headers)
 
-	jar.load(response.getheader("Set-Cookie", ""))
-	if cookie is not None:
-		for key, c in jar.items():
-			cookie[key] = c.value
-
-	b = response.read()
-
-	# decompress gziped data
-	if response.getheader("Content-Encoding") == "gzip":
-		b = gzip.decompress(b)
+	b = r.content
 
 	if raw:
-		content = b
-
-	else:
-		# find html defined encoding
-		s = b.decode("utf-8", "replace")
-		match = re.search(r"charset=[\"']?([^\"'>]+)", s)
-		if match:
-			s = b.decode(match.group(1), "replace")
-		content = s
-
-	if setting.getboolean("errorlog"):
-		log_object = (
-			url,
-			request.header_items(),
-			response.getheaders()
-		)
-		if not errorlog:
-			errorlog = ""
-		content_write("~/comiccrawler/grabber.log", pformat(log_object) + "\n\n", append=True)
-
-	return content
+		return b
+		
+	match = re.search(br"charset=[\"']?([^\"'>]+)", b)
+	if match:
+		r.encoding = match.group(1)
+		
+	return r.text
 
 def grabhtml(*args, **kwargs):
 	"""Get html source of given url. Return String."""
