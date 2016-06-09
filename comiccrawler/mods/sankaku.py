48db214163d6e122dc7a3ad958fe5547d5606cf2
eight <eight04@gmail.com>
2016-06-09 18:47:50 +0800

    Add login check

--- comiccrawler/mods/sankaku.py
+++ comiccrawler/mods/sankaku.py
@@ -5,6 +5,7 @@ from html import unescape
 from urllib.parse import urljoin
 
 from ..core import Episode, grabhtml
+from ..error import PauseDownloadError
 
 cookie = {}
 domain = ["chan.sankakucomplex.com"]
@@ -12,18 +13,25 @@ name = "Sankaku"
 noepfolder = True
 config = {
 	"cf_clearance": "Set cf_clearance",
-	"_sankakucomplex_session": "Set _sankakucomplex_session"
+	"_sankakucomplex_session": "Set _sankakucomplex_session",
+	"pass_hash": "Set pass_hash"
 }
+
+def login_check(html):
+	if '<a href="/user/login">' in html:
+		raise PauseDownloadError("You didn't login")
 
 def load_config():
 	cookie["cf_clearance"] = config["cf_clearance"]
 	cookie["_sankakucomplex_session"] = config["_sankakucomplex_session"]
+	cookie["pass_hash"] = config["pass_hash"]
 
 def get_title(html, url):
 	title = re.search(r"<title>/?(.+?) \|", html).group(1)
 	return "[sankaku] " + title
 
 def get_episodes(html, url):
+	login_check(html)
 	s = []
 	base = re.search("(https?://[^/]+)", url).group(1)
 	for m in re.finditer(r'href="(/(?:[^/]*/)?post/show/(\d+))"', html):
@@ -33,6 +41,7 @@ def get_episodes(html, url):
 	return s[::-1]
 
 def get_images(html, url):
+	login_check(html)
 	u = re.search('href="([^"]+)" id=highres', html)
 	if not u:
 		u = re.search('embed src="([^"]+)"', html)
