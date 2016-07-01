be2f88f23df01b799102a96211b83134bf5fcbd4
eight <eight04@gmail.com>
2016-07-02 03:27:48 +0800

    Release v2016.7.2

--- comiccrawler/__init__.py
+++ comiccrawler/__init__.py
@@ -24,7 +24,7 @@ Sub modules:
   comiccrawler.mods   Import download modules.
 """
 
-__version__ = "2016.7.1"
+__version__ = "2016.7.2"
 
 def console_download(url, savepath):
 	"""Download url to savepath."""
