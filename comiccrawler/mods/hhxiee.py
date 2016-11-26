94832d1d53773ee21661aa12e47681255691e1dc
eight <eight04@gmail.com>
2016-11-26 23:56:59 +0800

    Rewrite hhxiee. New domain hhssee.com

--- comiccrawler/mods/hhxiee.py
+++ comiccrawler/mods/hhxiee.py
@@ -9,45 +9,88 @@ Ex:
 
 import re
 import execjs
-import json
 
-from urllib.parse import urljoin, urlparse
+from urllib.parse import urljoin
 
 from ..core import Episode, grabhtml
 
-domain = ["www.hhxiee.com", "www.hhcomic.cc"]
+domain = ["www.hhxiee.com", "www.hhcomic.cc", "www.hhssee.com"]
 name = "汗汗"
 
 def get_title(html, url):
-	return re.search("<title>([^<]+)", html).group(1).partition(",")[0]
+	return re.search("<title>([^<]+)", html).group(1).strip().replace("漫画 - 汗汗漫画", "")
 	
 def get_episodes(html, url):
 	eps = []
 	
-	for match in re.finditer(r"href=(/xiee/[^>\s]+).+?>([^<]+)", html):
+	for match in re.finditer(r"href='(/page[^>\s']+).+?>([^<]+)", html):
 		ep_url, title = match.groups()
 		eps.append(Episode(title, urljoin(url, ep_url)))
 	return eps[::-1]
+	
+ctx = None
+def build_ctx(url):
+	"""Reuse javascript context"""
+	global ctx
+	js_url = urljoin(url, "/script/view.js")
+	js = grabhtml(js_url)
+	ctx = """
+		var imgEl = {
+				style: {},
+				name: ""
+			},
+			domainEl = {};
+			
+		location = {
+			href: "",
+			hostname: ""
+		};
+		document = {
+			location: location,
+			getElementById: function(id){
+				if (id == "hdDomain") {
+					return domainEl;
+				}
+				if (/^img/.test(id)) {
+					return imgEl;
+				}
+				return {};
+			}
+		};
+		window = {
+			document: document,
+			eval: eval,
+			parseInt: parseInt,
+			String: String,
+			RegExp: RegExp
+		};
+		function getImages(url, name, hdDomain) {
+			location.href = url;
+			location.hostname = url.match(/:\/\/([^\/]+)/)[1];
+			imgEl.name = name;
+			domainEl.value = hdDomain;
+			
+			window_onload();
+			return imgEl.src;
+		}
+	""" + js
+	ctx = execjs.compile(ctx)
 			
 def get_images(html, url):
-	# piclisturl
-	PicListUrl = re.search("var PicListUrl = .+?;", html).group()
-	js_url = urljoin(url, "/hh/8.js")
-	js = grabhtml(js_url)
-	ctx = execjs.compile(
-		"""
-		var location = {
-			href: """ + json.dumps(url) + """,
-			hostname: """ + json.dumps(urlparse(url).hostname) + """
-		};
-		var document = {
-			location: location
-		};
-		var window = {
-			document: document
-		};
-		""" + PicListUrl + js
-	)
-	server, imgs = ctx.eval("[ServerList[server-1], arrPicListUrl]")
-	return [server + i for i in imgs]
-			
+	if not ctx:
+		build_ctx(url)
+		
+	hdDomain = re.search('id="hdDomain" value="([^"]+)', html).group(1)
+	img_name = re.search('img[^>]+?name="([^"]+)', html).group(1)
+	
+	return ctx.call("getImages", url, img_name, hdDomain)
+	
+def get_next_page(html, url):
+	try:
+		count = int(re.search('id="hdPageCount" value="([^"]+)', html).group(1))
+		current = int(re.search('id="hdPageIndex" value="([^"]+)', html).group(1))
+	except AttributeError:
+		pass
+	else:
+		if current < count:
+			return re.sub(r"/\d+\.html", "/{}.html".format(current + 1), url)
