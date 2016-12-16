3c3bcadfb9ced1f6984a10a23663eb87cc3f85fe
eight <eight04@gmail.com>
2016-12-16 11:15:42 +0800

    Add migrate command

--- comiccrawler/__init__.py
+++ comiccrawler/__init__.py
@@ -6,12 +6,14 @@ Usage:
   comiccrawler domains
   comiccrawler download URL [--dest SAVE_FOLDER]
   comiccrawler gui
+  comiccrawler migrate
   comiccrawler (--help | --version)
 
 Commands:
   domains             List supported domains.
   download URL        Download from the URL.
   gui                 Launch TKinter GUI.
+  migrate             Convert old file path to new file path.
 
 Options:
   --dest SAVE_FOLDER  Set download save path. [default: .]
@@ -50,3 +52,36 @@ def console_init():
 
 	elif arguments["download"]:
 		console_download(arguments["URL"], arguments["--dest"])
+		
+	elif arguments["migrate"]:
+		migrate()
+
+def migrate():
+	import re
+	
+	from .mission_manager import mission_manager, get_mission_id
+	from .core import safefilepath
+	from .io import move
+	from .safeprint import print
+	
+	def safefilepath_old(s):
+		"""Return a safe directory name."""
+		return re.sub("[/\\\?\|<>:\"\*]","_",s).strip()
+		
+	def rename(l):
+		for src, dst in l:
+			if src != dst:
+				print("\n" + src + "\n" + dst)
+				move(src, dst)
+		
+	mission_manager.load()
+	to_rename = []
+	
+	for mission in mission_manager.pool.values():
+		id = get_mission_id(mission)
+		old_ep = "~/comiccrawler/pool/" + safefilepath_old(id + ".json")
+		new_ep = "~/comiccrawler/pool/" + safefilepath(id + ".json")
+		to_rename.append((old_ep, new_ep))
+		
+	rename(to_rename)
+	
