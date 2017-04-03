84b6d123fa1ed37f9243edca892ac291943bace4
eight <eight04@gmail.com>
2017-04-03 12:26:18 +0800

    Add url utils

---
+++ comiccrawler/url.py
@@ -0,0 +1,27 @@
+#! python3
+
+from urllib.parse import urlparse, urlunparse, parse_qs, urlencode, urljoin
+
+URL_PARTS = ("scheme", "netloc", "path", "params", "query", "fragment")
+
+def urlextract(url):
+	result = urlparse(url)
+	dict = {}
+	for prop in URL_PARTS:
+		dict[prop] = getattr(result, prop)
+	return dict
+	
+def urlbuild(d):
+	return urlunparse(d.get(p) for p in URL_PARTS)
+
+def urlupdate(url, **parts):
+	d = urlextract(url)
+	d.update(parts)
+	return urlbuild(d)
+	
+def update_qs(url, new_query):
+	d = urlextract(url)
+	query_dict = parse_qs(d["query"])
+	query_dict.update(new_query)
+	d["query"] = urlencode(query_dict, doseq=True)
+	return urlbuild(d)
