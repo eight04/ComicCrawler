93ac37064639e652b8f527f8ac3c421f48fb02f1
eight <eight04@gmail.com>
2017-05-29 00:06:29 +0800

    Fix 99

--- comiccrawler/mods/_99.py
+++ comiccrawler/mods/_99.py
@@ -1,17 +1,10 @@
 #! python3
 
-"""this is 99comic module for comiccrawler
-
-Ex:
-	http://www.99comic.com/comic/9910481/
-	
-"""
-
 import re
 from urllib.parse import urljoin
-from ..core import Episode
+from ..core import Episode, grabhtml
 
-domain = ["www.99comic.com"]
+domain = ["www.99comic.com", "99.hhxxee.com"]
 name = "99"
 
 def get_title(html, url):
@@ -25,25 +18,12 @@ def get_episodes(html, url):
 		s.append(Episode(title, urljoin(url, ep_url)))
 	return s[::-1]
 
-def get_images(html, url):	
-	ds = [
-		"http://218.24.35.163:9393/dm01/",
-		"http://218.24.35.163:9393/dm02/",
-		"http://218.24.35.163:9393/dm03/",
-		"http://218.24.35.163:9393/dm04/",
-		"http://218.24.35.163:9393/dm05/",
-		"http://218.24.35.163:9393/dm06/",
-		"http://218.24.35.163:9393/dm07/",
-		"http://218.24.35.163:9393/dm08/",
-		"http://218.24.35.163:9393/dm09/",
-		"http://218.24.35.163:9393/dm10/",
-		"http://112.123.174.23:9292/dm11/",
-		"http://218.24.35.163:9393/dm12/",
-		"http://218.24.35.163:9393/dm13/",
-		"http://173.231.57.238/dm14/",
-		"http://218.24.35.163:9393/dm15/",
-		"http://142.4.34.102/dm16/"
-	]
+def get_images(html, url):
+	viewhtml_js = re.search('src="([^"]+viewhtml\.js)"', html).group(1)
+	viewhtml_js = grabhtml(urljoin(url, viewhtml_js))
+	
+	ds = re.search('sDS = "([^"]+)', viewhtml_js).group(1)
+	ds = ds.split("|")
 	
 	sfiles = re.search("sFiles=\"([^\"]+)\"", html).group(1)
 	spath = re.search("sPath=\"(\d+)\"", html).group(1)
