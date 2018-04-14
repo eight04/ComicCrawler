e13539c51402fffb2b9400be7d5e584e59b8bbf7
eight <eight04@gmail.com>
2018-04-14 18:44:35 +0800

    Add: grab user page

--- comiccrawler/mods/weibo.py
+++ comiccrawler/mods/weibo.py
@@ -4,26 +4,46 @@
 
 Ex:
 	http://tw.weibo.com/2362470852/4061673218817198
+	http://tw.weibo.com/wulazula
 
 """
 
 import re
 
 from ..core import Episode
+from ..url import urljoin
 
 domain = ["tw.weibo.com"]
 name = "weibo"
 noepfolder = True
 
 def get_title(html, url):
+	match = re.search("^http://tw\.weibo\.com/(\w+)$", url)
+	if match:
+		user_id = match.group(1)
+		user_name = re.search('class="name">\s*<h3><a[^>]+>([^<]+)', html).group(1)
+		return "[weibo] {user_id} - {user_name}".format(user_id=user_id, user_name=user_name)
 	title = re.search('<title>([^<]+)', html).group(1)
 	title = title.replace("- 微博精選 - 微博台灣站", "").replace("\n", "").strip()
 	id = re.search('tw\.weibo\.com/([^/]+/\d+)', url).group(1)
 	return "[weibo] {title} ({id})".format(title=title, id=id)
 
 def get_episodes(html, url):
-	return [Episode("image", url)]
+	if re.match("http://tw\.weibo\.com/\w+/\d+$", url):
+		return [Episode("image", url)]
+		
+	s = []
+	pattern = 'class="img_link" href="(http://tw.weibo.com/(\w+/\d+))">'
+	for match in re.finditer(pattern, html):
+		ep_url, ep_title = match.groups()
+		s.append(Episode(ep_title, ep_url))
+	return s[::-1]
 
 def get_images(html, url):
 	images = re.findall('<img src="([^"]+?sinaimg\.cn/bmiddle[^"]+)', html)
 	return map(lambda i: i.replace("/bmiddle/", "/large/"), images)
+	
+def get_next_page(html, url):
+	match = re.search('class="pgNext"><a href="([^"]+)"', html)
+	if match:
+		return urljoin(url, match.group(1))
