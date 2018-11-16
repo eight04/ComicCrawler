6187d6f1f009006525d91402bd9bafe054d8583c
eight <eight04@gmail.com>
2018-11-17 02:17:31 +0800

    Fix: qq

--- comiccrawler/mods/qq.py
+++ comiccrawler/mods/qq.py
@@ -4,11 +4,11 @@
 http://ac.qq.com/Comic/comicInfo/id/626619
 """
 
-import base64
-import json
 import re
+import node_vm2
 
 from ..core import Episode
+from ..grabber import grabhtml
 from ..url import urljoin
 
 domain = ["ac.qq.com"]
@@ -32,7 +32,16 @@ def get_episodes(html, url):
 	return episodes
 	
 def get_images(html, url):
-	data = re.search("var DATA\s*=\s*'.([^']+)", html).group(1)
-	data = base64.b64decode(data).decode("latin-1")
-	data = json.loads(data)
+	data = re.search("var DATA\s*=\s*'[^']+'", html).group()
+	nonce = re.search("window\.nonce = (.+)", html).group(1)
+	nonce2 = re.search("window\[.+?=(.+)", html)
+	nonce2 = nonce2.group(1) if nonce2 else None
+	
+	view_js = re.search('src="([^"]+?page\.chapter\.view[^"]+?\.js[^"]+)', html).group(1)
+	view_js = grabhtml(urljoin(url, view_js))
+	view_js = re.search("(eval\(.+?)\}\(\)", view_js, re.DOTALL).group(1)
+	
+	code = data + ";var nonce = " + (nonce2 or nonce) + ";var W = {DATA, nonce};" + view_js + ";_v"
+	
+	data = node_vm2.eval(code)
 	return [p["url"] for p in data["picture"]]
