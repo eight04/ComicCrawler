9ea68339b10b56a9f3626a29c2e06602f1aef199
eight <eight04@gmail.com>
2019-03-18 22:10:05 +0800

    Fix: handle 403 error (#182)

--- comiccrawler/mods/artstation.py
+++ comiccrawler/mods/artstation.py
@@ -13,7 +13,7 @@ from node_vm2 import eval
 
 from ..url import urljoin, urlparse, parse_qs, update_qs
 from ..core import Episode
-from ..error import SkipPageError
+from ..error import SkipPageError, is_http
 
 domain = ["www.artstation.com"]
 name = "ArtStation"
@@ -29,7 +29,7 @@ def is_project(url):
 	
 def is_user_home(url):
 	return re.search("www\.artstation\.com/[^/]+$", url)
-
+	
 def get_episodes(html, url):
 	if is_project(url):
 		return [Episode(
@@ -39,9 +39,15 @@ def get_episodes(html, url):
 	raise SkipPageError
 
 def get_images(html, url):
-	hash = re.search("/artwork/([^/]+)", url).group(1)
-	pattern = "cache\.put\('/projects/{hash}\.json', ('.+?')\);$".format(hash=hash)
-	data_json = eval(re.search(pattern, html, re.M).group(1))
+	match = re.search("/artwork/([^/]+)", url)
+	if match:
+		hash = match.group(1)
+		pattern = "cache\.put\('/projects/{hash}\.json', ('.+?')\);$".format(hash=hash)
+		data_json = eval(re.search(pattern, html, re.M).group(1))
+	elif re.search("/projects/(\w+)\.json", url):
+		data_json = html
+	else:
+		raise Exception("Unknown page {}".format(url))
 	data = json.loads(data_json)
 	return [a["image_url"] for a in data["assets"]]
 	
@@ -54,3 +60,12 @@ def get_next_page(html, url):
 	if is_user_home(url):
 		user = re.search("www\.artstation\.com/([^/]+)", url).group(1)
 		return urljoin(url, "/users/{user}/projects.json?page=1".format(user=user))
+
+def errorhandler(err, crawler):
+	if not is_http(err, 403) or not crawler.ep.current_url:
+		return
+	match = re.search("artwork/([^/]+)", crawler.ep.current_url)
+	if not match:
+		return
+	artwork = match.group(1)
+	crawler.ep.current_url = urljoin(crawler.ep.current_url, "/projects/{}.json".format(artwork))
