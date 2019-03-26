6d6540c209b4ef954a469b590528b53792e5ca46
eight <eight04@gmail.com>
2019-03-26 18:50:41 +0800

    Add: manhuaren module (#186)

---
+++ comiccrawler/mods/manhuaren.py
@@ -0,0 +1,33 @@
+#! python3
+
+"""
+https://www.manhuaren.com/manhua-mofashaonvshenmedeyijinggoulela/
+"""
+
+import json
+import re
+from html import unescape
+from urllib.parse import urljoin
+
+from node_vm2 import eval
+
+from ..core import Episode
+
+domain = ["www.manhuaren.com"]
+name = "漫畫人"
+
+def get_title(html, url):
+	name = re.search('DM5_COMIC_MNAME=("[^"]+")', html).group(1)
+	return json.loads(name)
+
+def get_episodes(html, url):
+	s = []
+	pattern = 'href="(/m\d+/)[^>]+chapteritem[^>]+>([^<]+)'
+	for match in re.finditer(pattern, html):
+		ep_url, title = match.groups()
+		s.append(Episode(unescape(title), urljoin(url, ep_url)))
+	return s[::-1]
+	
+def get_images(html, url):
+	js = re.search(r"(eval\([\s\S]+?)</script", html).group(1)
+	return eval(js + ";newImgs")
