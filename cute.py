8a99a1508b15e6598832500542b27344e895c589
eight <eight04@gmail.com>
2019-11-03 20:04:36 +0800

    Change: bump pyxcute, drop livereload

--- cute.py
+++ cute.py
@@ -1,14 +1,7 @@
 #! python3
 
-from xcute import cute, Bump
+from xcute import cute, Bump, LiveReload
 
-def readme():
-	"""Live reload readme"""
-	from livereload import Server
-	server = Server()
-	server.watch("README.rst", "py cute.py readme_build")
-	server.serve(open_url_delay=1, root="build/readme")
-	
 def date_bumper(old_version):
 	"""My bump task"""
 	from datetime import datetime
@@ -67,5 +60,5 @@ cute(
 			'build/readme/index.rst build/readme/index.html'
 	],
 	readme_pre = "readme_build",
-	readme = readme,
+	readme = LiveReload("README.rst", "readme_build", "build/readme")
 )
