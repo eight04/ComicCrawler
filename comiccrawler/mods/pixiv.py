bb61e19e5bf2128197212030175657062334ad21
eight <eight04@gmail.com>
2019-11-12 20:54:40 +0800

    Fix: globalInitData is not used anymore in pixiv (#228)
    * Fix: unused globalInitData
    
    * Fix: html.escape is undefined
    
    * Add: allow get_images to throw SkipPageError

--- comiccrawler/mods/pixiv.py
+++ comiccrawler/mods/pixiv.py
@@ -14,10 +14,8 @@ from io import BytesIO
 from urllib.parse import urljoin, urlencode, urlparse, parse_qs
 from zipfile import ZipFile
 
-from node_vm2 import eval
-
 from ..core import Episode, grabhtml
-from ..error import PauseDownloadError, is_http, SkipEpisodeError
+from ..error import PauseDownloadError, is_http, SkipEpisodeError, SkipPageError
 from ..safeprint import print
 
 domain = ["www.pixiv.net"]
@@ -27,13 +25,21 @@ config = {
 	"cookie_PHPSESSID": "請輸入Cookie中的PHPSESSID"
 }
 
+class DataNotFound(Exception):
+	pass
+
 def get_init_data(html):
-	js = re.search("(var globalInitData =.+?)</script>", html, re.DOTALL).group(1)
-	return eval("""
-	Object.freeze = n => n;
-	""" + js + """
-	globalInitData;
-	""")
+	match = re.search("""meta-global-data" content='([^']+)""", html)
+	if not match:
+		raise DataNotFound
+	data = json.loads(unescape(match.group(1)))
+		
+	match = re.search("""meta-preload-data" content='([^']+)""", html)
+	# if not match:
+		# raise DataNotFound
+	data["preload"] = json.loads(unescape(match.group(1)))
+	
+	return data
 
 def get_title_from_init_data(html, url):
 	init_data = get_init_data(html)
@@ -43,9 +49,10 @@ def get_title_from_init_data(html, url):
 	return "{} - {}{}".format(user["userId"], user["name"], tag)
 
 def get_title(html, url):
-	if "globalInitData" in html:
+	try:
 		return get_title_from_init_data(html, url)
-	return "[pixiv] " + unescape(re.search("<title>([^<]+)", html).group(1))
+	except DataNotFound:
+		return "[pixiv] " + unescape(re.search("<title>([^<]+)", html).group(1))
 	
 def check_login(data):
 	if not data.get("userData"):
@@ -69,63 +76,75 @@ def get_episodes_from_works(works):
 def get_tag_from_url(url):
 	tags = parse_qs(urlparse(url).query).get("tag")
 	return tags[0] if tags else None
+	
+def get_episodes_from_init_data(html, url):
+	init_data = get_init_data(html)
+	check_login(init_data)
+	
+	id = int(next(iter(init_data["preload"]["user"])))
+	tag = get_tag_from_url(url)
+	
+	if tag:
+		def build_url(offset):
+			query = {
+				"offset": str(offset),
+				"limit": "48",
+				"tag": tag
+			}
+			return "https://www.pixiv.net/ajax/user/{}/illustmanga/tag?{}".format(id, urlencode(query))
+			
+		response = grabhtml(build_url(0))
+		response = json.loads(response)
+		total = response["body"]["total"]
+		i = 48
+		pre_url = url
+		while i < total:
+			next_url = build_url(i)
+			cache_next_page[pre_url] = next_url
+			pre_url = next_url
+			i += 48
+		return get_episodes_from_works(response["body"]["works"])
+	
+	all = grabhtml("https://www.pixiv.net/ajax/user/{}/profile/all".format(id))
+	all = json.loads(all)
+	
+	ep_ids = [int(id) for id in list(all["body"]["illusts"]) + list(all["body"]["manga"])]
+	ep_ids.sort()
+	ep_ids.reverse()
+	
+	pre_url = url
+	for page, i in enumerate(range(0, len(ep_ids), 48)):
+		ids = ep_ids[i:i + 48]
+		query = [("ids[]", str(id)) for id in ids] + [
+			("is_manga_top", "0"),
+			("work_category", "illustManga"),
+			("is_first_page", "1" if page == 0 else "0")
+		]
+		new_url = "https://www.pixiv.net/ajax/user/{}/profile/illusts?{}".format(
+			id, urlencode(query))
+		cache_next_page[pre_url] = new_url
+		pre_url = new_url
+	raise SkipPageError
+	
+def get_episodes_from_ajax_result(html, url):
+	if "ajax/user" not in url:
+		raise DataNotFound
+	works = json.loads(html)["body"]["works"]
+	if isinstance(works, dict):
+		works = works.values()
+	return get_episodes_from_works(works)
 		
 def get_episodes(html, url):
-	if "ajax/user" in url:
-		works = json.loads(html)["body"]["works"]
-		if isinstance(works, dict):
-			works = works.values()
-		return get_episodes_from_works(works)
+	try:
+		return get_episodes_from_ajax_result(html, url)
+	except DataNotFound:
+		pass
+		
+	try:
+		return get_episodes_from_init_data(html, url)
+	except DataNotFound:
+		pass
 
-	if "globalInitData" in html:
-		init_data = get_init_data(html)
-		check_login(init_data)
-		
-		id = int(next(iter(init_data["preload"]["user"])))
-		tag = get_tag_from_url(url)
-		
-		if tag:
-			def build_url(offset):
-				query = {
-					"offset": str(offset),
-					"limit": "48",
-					"tag": tag
-				}
-				return "https://www.pixiv.net/ajax/user/{}/illustmanga/tag?{}".format(id, urlencode(query))
-				
-			response = grabhtml(build_url(0))
-			response = json.loads(response)
-			total = response["body"]["total"]
-			i = 48
-			pre_url = url
-			while i < total:
-				next_url = build_url(i)
-				cache_next_page[pre_url] = next_url
-				pre_url = next_url
-				i += 48
-			return get_episodes_from_works(response["body"]["works"])
-		
-		all = grabhtml("https://www.pixiv.net/ajax/user/{}/profile/all".format(id))
-		all = json.loads(all)
-		
-		ep_ids = [int(id) for id in list(all["body"]["illusts"]) + list(all["body"]["manga"])]
-		ep_ids.sort()
-		ep_ids.reverse()
-		
-		pre_url = url
-		for page, i in enumerate(range(0, len(ep_ids), 48)):
-			ids = ep_ids[i:i + 48]
-			query = [("ids[]", str(id)) for id in ids] + [
-				("is_manga_top", "0"),
-				("work_category", "illustManga"),
-				("is_first_page", "1" if page == 0 else "0")
-			]
-			new_url = "https://www.pixiv.net/ajax/user/{}/profile/illusts?{}".format(
-				id, urlencode(query))
-			cache_next_page[pre_url] = new_url
-			pre_url = new_url
-		return []
-	
 	check_login_html(html)
 	s = []
 	# search result?
@@ -150,6 +169,12 @@ def get_nth_img(url, i):
 	return re.sub(r"_p0(\.\w+)$", r"_p{}\1".format(i), url)
 
 def get_images(html, url):
+	if "&amp;" in url:
+		# fix bad URL for old saves e.g. 
+		# https://www.pixiv.net/member_illust.php?mode=medium&amp;illust_id=12345
+		cache_next_page[url] = unescape(url)
+		raise SkipPageError
+
 	init_data = get_init_data(html)
 	check_login(init_data)
 	illust_id = re.search("illust_id=(\d+)", url).group(1)
