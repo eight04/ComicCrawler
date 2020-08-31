aedff175d3fa3240fe4d3ccd114f187b4f2f8777
eight04 <eight04@gmail.com>
2020-08-31 20:00:27 +0800

    Fix: sankaku api is changed

--- comiccrawler/mods/sankaku_beta.py
+++ comiccrawler/mods/sankaku_beta.py
@@ -13,8 +13,6 @@ domain = ["beta.sankakucomplex.com"]
 name = "Sankaku Beta"
 noepfolder = True
 
-data_len_cache = {}
-
 class ExpireError(Exception):
 	pass
 
@@ -25,37 +23,42 @@ def get_query(url, name):
 def get_title(html, url):
 	return "[sankaku] {}".format(get_query(url, "tags"))
 
+next_page_cache = {}
+
 def get_episodes(html, url):
 	if re.match(r"https://beta\.sankakucomplex\.com/\?", url):
+		next_page_cache[url] = update_qs("https://capi-v2.sankakucomplex.com/posts/keyset?lang=en&default_threshold=1&hide_posts_in_books=never&limit=40", {
+			"tags": get_query(url, "tags")
+		})
 		raise SkipPageError
 		
 	data = json.loads(html)
-	data_len_cache[url] = len(data)
-	return [
+	next = data["meta"]["next"]
+	# data_len_cache[url] = len(data)
+	eps = [
 		Episode(
 			str(e["id"]),
 			"https://beta.sankakucomplex.com/post/show/{}".format(e["id"]),
 			image=e["file_url"]
-		) for e in reversed(data)
+		) for e in data["data"]
 	]
+	
+	if next:
+		next_page_cache[url] = update_qs(url, {
+			"next": next
+		})
+	
+	return eps[::-1]
 
 def get_images(html, url):
 	id = re.search("post/show/(\d+)", url).group(1)
 	data = grabhtml("https://capi-v2.sankakucomplex.com/posts?lang=english&page=1&limit=1&tags=id_range:{}".format(id))
 	data = json.loads(data)
 	return data[0]["file_url"]
-
+	
 def get_next_page(html, url):
-	if re.match(r"https://beta\.sankakucomplex\.com/\?", url):
-		return update_qs("https://capi-v2.sankakucomplex.com/posts?lang=english&page=1&limit=40", {
-			"tags": "hide_posts_in_books:never {}".format(get_query(url, "tags"))
-		})
-		
-	if "https://capi-v2.sankakucomplex.com/posts" in url:
-		if data_len_cache[url] >= 40:
-			return update_qs(url, {
-				"page": int(get_query(url, "page")) + 1
-			})
+	if url in next_page_cache:
+		return next_page_cache.pop(url)
 			
 def redirecthandler(response, crawler):
 	if re.match(r"https://chan\.sankakucomplex\.com/.+\.(png|jpg)", response.url):
