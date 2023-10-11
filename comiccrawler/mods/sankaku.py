86179712ccc5638dc14af03f5399c05ab3c9ac11
eight04 <eight04@gmail.com>
2023-10-11 08:55:32 +0800

    Fix: sanka

--- comiccrawler/mods/sankaku.py
+++ comiccrawler/mods/sankaku.py
@@ -5,7 +5,7 @@ from html import unescape
 from urllib.parse import urlparse, parse_qs, quote, urljoin
 
 from ..core import Episode
-from ..error import PauseDownloadError
+from ..error import PauseDownloadError, SkipEpisodeError
 
 domain = ["chan.sankakucomplex.com"]
 name = "Sankaku"
@@ -46,11 +46,15 @@ def get_episodes(html, url):
 	return s[::-1]
 
 def get_images(html, url):
+	if "This post was deleted" in html:
+		raise SkipEpisodeError(always=True)
 	login_check(html)
-	u = re.search('href="([^"]+)" id=highres', html)
-	if not u:
-		u = re.search('embed src="([^"]+)"', html)
-	return ["https:" + unescape(u.group(1))]
+	result = ""
+	if match := re.search('<a [^>]*highres[^>]*>', html):
+		result = re.search('href="([^"]+)"', match.group(0)).group(1)
+	elif match := re.search('embed src="([^"]+)"', html):
+		result = match.group(1)
+	return [urljoin(url, unescape(result))]
 
 def get_next_page(html, url):
 	if url in next_page_cache:
