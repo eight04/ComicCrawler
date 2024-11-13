02d7538d8810425d68c17b14319d424e649f42fc
eight04 <eight04@gmail.com>
2024-11-14 01:49:51 +0800

    Bump deps

--- cute.py
+++ cute.py
@@ -54,7 +54,7 @@ cute(
 	],
 	readme_build = [
 		'python setup.py --long-description | x-pipe build/readme/index.rst',
-		'rst2html5.py --no-raw --exit-status=1 --verbose '
+		'rst2html5 --no-raw --exit-status=1 --verbose '
 			'build/readme/index.rst build/readme/index.html'
 	],
 	readme_pre = "readme_build",
