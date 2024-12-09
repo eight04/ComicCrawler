fa8e2413b8a7ec853f558617cf04b9ae227bb44d
eight04 <eight04@gmail.com>
2024-12-09 17:48:37 +0800

    Fix: login check in exh. fixes #395

--- comiccrawler/mods/exh.py
+++ comiccrawler/mods/exh.py
@@ -25,26 +25,35 @@ config = {
 
 def get_boolean(s):
 	return ConfigParser.BOOLEAN_STATES.get(s.lower())
+
+def has_login(html, page="ep"):
+	if not html:
+		return False
+	if html[6:10] == "JFIF":
+		return False
+	if page == "ep" and ("mytags" not in html and "home.php" not in html):
+		return False
+	return True
 	
-def check_login(html):
-	if html[6:10] == "JFIF" or ("mytags" not in html and "home.php" not in html):
+def check_login(html, page="ep"):
+	if not has_login(html, page):
 		raise PauseDownloadError("You didn't login!")
 
 def get_title(html, url):
-	check_login(html)
+	check_login(html, page="ep")
 	title = re.findall("<h1 id=\"g(j|n)\">(.+?)</h1>", html)[-1][1]
 	hash = re.search(r"/g/(\w+/\w+)", url).group(1)
 	return unescape("{title} ({hash})".format(title=title, hash=hash))
 
 def get_episodes(html, url):
-	check_login(html)
+	check_login(html, page="ep")
 	url = re.search(r'href="([^\"]+?/s/\w+/\w+-1)"', html).group(1)
 	e = Episode("image", url)
 	return [e]
 
 nl = ""
 def get_images(html, url):
-	check_login(html)
+	check_login(html, page="img")
 	global nl
 	nl = re.search(r"nl\('([^)]+)'\)", html).group(1)
 	
@@ -73,7 +82,8 @@ def errorhandler(er, crawler):
 	crawler.html = None
 
 def get_next_page(html, url):
-	check_login(html)
+	page = "img" if "/s/" in url else "ep"
+	check_login(html, page=page)
 	match = re.search('id="next"[^>]+?href="([^"]+)', html)
 	if match:
 		match = match.group(1)
