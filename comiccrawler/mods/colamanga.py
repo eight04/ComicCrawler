506cd3d6c4dbc17cb5776deda09fc96d39af45a8
eight04 <eight04@gmail.com>
2025-03-24 21:40:31 +0800

    Change: use only one vm

--- comiccrawler/mods/colamanga.py
+++ comiccrawler/mods/colamanga.py
@@ -13,11 +13,11 @@ from html import unescape
 # import time
 from urllib.parse import urljoin
 
-from deno_vm import eval
+from Cryptodome.Cipher import AES
+from deno_vm import VM
 
 from ..core import Episode, grabhtml
 from ..session_manager import session_manager
-from Cryptodome.Cipher import AES
 
 domain = ["www.colamanga.com"]
 name = "colamanga"
@@ -80,7 +80,7 @@ def get_images(html, url):
 		html, url, scripts=[r"\/l\.js", r"common\.js", r"custom\.js", r"manga\.read\.js"]
 	)
 
-	code = ("""
+	code = """
 
 const _log = console.log;
 
@@ -107,13 +107,13 @@ self.convertWordArrayToUint8Array =
 	undefined;
 
 	(function() {
-	
+
 		let _cookies = "";
 
 		function noop(path = "") {
 			if (path === "document.cookie") return _cookies;
 			if (path === "$.inArray") return (v, a) => a.indexOf(v);
-			
+
 			return new Proxy(() => {}, {
 				apply: () => noop(`${path}.called`),
 				get: (target, prop) => {
@@ -181,209 +181,45 @@ self.convertWordArrayToUint8Array =
 			imgs.push(__cr.getPicUrl(i));
 		}
 		const keyTypes = image_info.keyType
+		let key;
 
-		const res = {};
-		res["imgs"] = imgs;
-		res["keyTypes"] = keyTypes;
+		if (keyTypes == 0) {
+			__cr.isfromMangaRead = 1;
+			key =  __js.getDataParse();
+		}
 
-		return res
+		self.getKey = (fn, index) => {
+			const hexKey = eval(`${fn}(${index})`);
+			return CryptoJS.enc.Utf8.parse(hexKey);
+		}
+
+		return [imgs, key, keyTypes]
 	}).call(self);
-	""")
+	"""
 
-	# import pathlib
-	# pathlib.Path("colamanaga.mjs").write_text(code, encoding="utf-8")
-	imgs, key_types = eval(code).values()
+	with VM() as vm:
+		imgs, key, key_types = vm.run(code)
 
-	data["key_types"] = key_types
-	data["url"] = url
-	data["cdata"] = cdata
-
-	return [urljoin(url, i) for i in imgs]
-
-
-def imagehandler(ext, bin):
-	if ext == ".webp":
-		key = None
-
-		if data["key_types"] == "0":
-			code = ("""
-
-			const _log = console.log;
-
-			Function.prototype.toString = (function(_toString) {
-				return function() {
-					return _toString.apply(this, arguments).replace(/\\r?\\n/g, '');
-				}
-			})(Function.prototype.toString);
-
-			self.setInterval = function() {};
-
-			self.eval = function(_eval) {
-			return function() {
-				_log('eval', arguments[0]);
-				return _eval.apply(this, arguments);
-			};
-			}(self.eval);
-
-			self.convertWordArrayToUint8Array =
-				self.convertUint8ArrayToWordArray =
-				self.__b_a =
-				self.__cad = 
-				self.__js = 
-				undefined;
-
-			(function() {
-
-				let _cookies = "";
-
-				function noop(path = "") {
-					if (path === "document.cookie") return _cookies;
-					if (path === "$.inArray") return (v, a) => a.indexOf(v);
-
-					return new Proxy(() => {}, {
-						apply: () => noop(`${path}.called`),
-						get: (target, prop) => {
-							const propPath = typeof prop == "symbol" ? `${path}.${String(prop)}` : `${path}.${prop}`;
-							if (propPath == "document.domain") return "www.colamanga.com";
-							_log("get", propPath);
-							return noop(propPath);
-						},
-						set: (target, prop, value) => {
-							const propPath = `${path}.${prop}`;
-							if (propPath == "document.cookie") {
-							_cookies += value.split(";")[0] + "; ";
-							}
-							_log(propPath, value);
-							return value;
-						}
-					});
-				}
-
-				self.window = self;
-				self.location = {
-					protocol: "http://",
-					href: '""" + data["url"] + """'
-				}
-				self.navigator = {
-					userAgent: ""
-				};
-				self.document = noop("document")
-				self.$ = noop("$");
-				self.devtools = noop("devtools");
-				self.localStorage = noop("localStorage");
-
-				self.C_DATA = """ + data["cdata"] + "\n" + str(scripts) + """
-
-				window.use_domain = {
-				},
-				window.lines = {
-					[mh_info.pageid]: {
-						use_line: mh_info.domain
-					}
-				};
-				window.chapter_id = mh_info.pageid;
-
-				__cr.isfromMangaRead = 1;
-				const key =  __js.getDataParse();
-				return key;
-			}).call(self);
-			""")
-
-			key = eval(code, console='inherit')
-		else:
+		if not key:
 			key_pairs = re.findall(r"if\(_0x[0-9a-f]{6}==(0x[0-9a-f]{4,})\)return _0x[0-9a-f]{6}\((0x[0-9a-f]+)\)", scripts[r"manga\.read\.js"])
 
-			pair = next((t for t in key_pairs if hex(int(data["key_types"])) in t), None)
+			pair = next((t for t in key_pairs if hex(int(key_types)) in t), None)
 			if pair is None:
 				raise ValueError("Key pair not found!")
 			index = pair[1]
 
 			fn_name = re.search(r"function (_0x[0-9a-f]{4})\(_0x[0-9a-f]{6},_0x[0-9a-f]{6}\)", scripts[r"manga\.read\.js"]).group(1)
 
-			code = ("""
+			key = vm.call("getKey", fn_name, index)
 
-			const _log = console.log;
+	data["key"] = key
 
-			Function.prototype.toString = (function(_toString) {
-				return function() {
-					return _toString.apply(this, arguments).replace(/\\r?\\n/g, '');
-				}
-			})(Function.prototype.toString);
+	return [urljoin(url, i) for i in imgs]
 
-			self.setInterval = function() {};
 
-			self.eval = function(_eval) {
-			return function() {
-				_log('eval', arguments[0]);
-				return _eval.apply(this, arguments);
-			};
-			}(self.eval);
-
-			self.convertWordArrayToUint8Array =
-				self.convertUint8ArrayToWordArray =
-				self.__b_a =
-				self.__cad = 
-				self.__js = 
-				undefined;
-
-			(function() {
-
-				let _cookies = "";
-
-				function noop(path = "") {
-					if (path === "document.cookie") return _cookies;
-					if (path === "$.inArray") return (v, a) => a.indexOf(v);
-
-					return new Proxy(() => {}, {
-						apply: () => noop(`${path}.called`),
-						get: (target, prop) => {
-							const propPath = typeof prop == "symbol" ? `${path}.${String(prop)}` : `${path}.${prop}`;
-							if (propPath == "document.domain") return "www.colamanga.com";
-							_log("get", propPath);
-							return noop(propPath);
-						},
-						set: (target, prop, value) => {
-							const propPath = `${path}.${prop}`;
-							if (propPath == "document.cookie") {
-							_cookies += value.split(";")[0] + "; ";
-							}
-							_log(propPath, value);
-							return value;
-						}
-					});
-				}
-
-				self.window = self;
-				self.location = {
-					protocol: "http://",
-					href: '""" + data["url"] + """'
-				}
-				self.navigator = {
-					userAgent: ""
-				};
-				self.document = noop("document")
-				self.$ = noop("$");
-				self.devtools = noop("devtools");
-				self.localStorage = noop("localStorage");
-
-				self.C_DATA = """ + data["cdata"] + "\n" + str(scripts) + """
-
-				window.use_domain = {
-				},
-				window.lines = {
-					[mh_info.pageid]: {
-						use_line: mh_info.domain
-					}
-				};
-				window.chapter_id = mh_info.pageid;
-
-				const hexKey = """ + fn_name + """(""" + index + """);""" + """
-				const key = CryptoJS.enc.Utf8.parse(hexKey);
-				return key;
-			}).call(self);
-			""")
-
-			key = eval(code)
+def imagehandler(ext, bin):
+	if ext == ".webp":
+		key = data["key"]
 
 		if not key:
 			raise ValueError("Key not found!")
@@ -393,84 +229,3 @@ def imagehandler(ext, bin):
 		bin = cipher.decrypt(bytes(bin))
 	return ext, bin
 
-
-# def suboptimal_imagehandler(ext, bin):
-# 	 script = scripts[r"\/l\.js"]
-# 	 uint8_array = list(bin)
-# 	 code = f"""
-# 	 (function() {{
-# 		 {script}
-# 		 function convertUint8ArrayToBinaryString(u8Array) {{
-# 			 var i, len = u8Array.length, b_str = "";
-# 			 for (i=0; i<len; i++) {{
-# 				 b_str += String.fromCharCode(u8Array[i]);
-# 			 }}
-# 			 return b_str;
-# 		 }}
-
-# 		 const uint8Array = new Uint8Array({uint8_array});
-# 		 console.log("uint8Array length: ", uint8Array.length);
-# 		 console.log("uint8Array first 10: ", JSON.stringify(uint8Array.slice(0, 10)));
-
-# 		 const wordArray = convertUint8ArrayToWordArray(uint8Array);
-# 		 console.log("wordArray 'sigBytes': ", wordArray["sigBytes"]);
-# 		 console.log("wordArray 'words' length: ", wordArray["words"].length);
-# 		 console.log("wordArray 'words' first 10: ", wordArray["words"].slice(0, 10));
-
-# 		 const key = {key}
-# 		 console.log("key: ", JSON.stringify(key));
-
-# 		 const encryptedWA = {{
-# 			 "ciphertext": wordArray
-# 		 }}
-# 		 const decryptedWA = CryptoJS.AES.decrypt(
-# 			 encryptedWA,
-# 			 key,
-# 			 {{
-# 				 iv: CryptoJS.enc.Utf8.parse("0000000000000000"),
-# 				 mode: CryptoJS.mode.CBC,
-# 				 padding: CryptoJS.pad.Pkcs7,
-# 			 }}
-# 		 );
-# 		 console.log("decryptedWA 'sigBytes':", decryptedWA["sigBytes"]);
-# 		 console.log("decryptedWA 'words' length:", decryptedWA["words"].length);
-# 		 console.log("decryptedWA 'words' first 10:", decryptedWA["words"].slice(0, 10));
-
-# 		 function convertUint8ArrayToBinaryString(u8Array) {{
-# 			 var i, len = u8Array.length, b_str = "";
-# 			 for (i=0; i<len; i++) {{
-# 				 b_str += String.fromCharCode(u8Array[i]);
-# 			 }}
-# 			 return b_str;
-# 		 }}
-
-# 		 const decryptedUint8Array = convertWordArrayToUint8Array(decryptedWA);
-# 		 // const arr = Array.from(decryptedUint8Array);
-# 		 // return arr;
-
-# 		 const bin = convertUint8ArrayToBinaryString(decryptedUint8Array);
-# 		 return bin
-# 	 }}).call(self);
-# 	 """
-
-# 	 # start = time.perf_counter()
-# 	 # result = eval(code, console="inherit")
-# 	 result = eval(code)
-# 	 # elapsed = time.perf_counter() - start
-
-# 	 # print("Begin log result:")
-# 	 # print("result: ", result)
-# 	 # print("result type: ", type(result))
-# 	 # print("Time: ", elapsed)
-
-# 	 ext = ".webp"
-# 	 bin = bytes(ord(c) for c in result)
-
-# 	 # import pathlib
-# 	 # pathlib.Path("debug.webp").write_bytes(bin)
-
-# 	 return ext, bin
-
-
-# def errorhandler(error, crawler):
-# 	raise SkipEpisodeError()
